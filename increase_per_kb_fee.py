c6899a62c673ada65634631e31b08695a47bef8f
Fabian Schuh <mail@xeroc.org>
2016-01-21 12:54:45 +0100

    Improve sccript .. only change too low fees

--- increase_per_kb_fee.py
+++ increase_per_kb_fee.py
@@ -1,9 +1,10 @@
 from grapheneapi.grapheneclient import GrapheneClient
-from graphenebase.transactions import getOperationNameForId, operations
+from graphenebase.transactions import getOperationNameForId
 import json
+from deepdiff import DeepDiff
 
 proposer   = "xeroc"
-expiration = "2016-01-21T23:59:59"
+expiration = "2016-01-21T22:59:59"
 price_per_kbyte = 40  # in BTS
 broadcast = False
 
@@ -16,26 +17,26 @@ if __name__ == '__main__':
     graphene = GrapheneClient(Wallet)
     obj = graphene.getObject("2.0.0")
     current_fees = obj["parameters"]["current_fees"]["parameters"]
+    old_fees = obj["parameters"]["current_fees"]
     scale = obj["parameters"]["current_fees"]["scale"] / 1e4
 
     # General change of parameter
     changes = {}
     for f in current_fees :
-        if ("price_per_kbyte" in f[1]):
-            # Set the current fees
-            changes[getOperationNameForId(f[0])] = f[1]
-            # update the price_per_kbyte
+        if ("price_per_kbyte" in f[1]) and (f[1]["price_per_kbyte"] is 20):
+            print("Changing operation %s[%d]" % (getOperationNameForId(f[0]), f[0]))
+            changes[getOperationNameForId(f[0])] = f[1].copy()
             changes[getOperationNameForId(f[0])]["price_per_kbyte"] = int(price_per_kbyte / scale * 1e5)
 
     # overwrite / set specific fees
-    changes["transfer"]["price_per_kbyte"]       = int(  20 / scale * 1e5)
-    changes["account_update"]["price_per_kbyte"] = int(   5 / scale * 1e5)
+    # changes["transfer"]["price_per_kbyte"]       = int(  20 / scale * 1e5)
+    # changes["account_update"]["price_per_kbyte"] = int(   5 / scale * 1e5)
 
     print("=" * 80)
-    print(json.dumps(changes, indent=4))
-    print("=" * 80)
     tx = graphene.rpc.propose_fee_change(proposer, expiration, changes, broadcast)
-    print(json.dumps(tx["operations"][0][1]["proposed_ops"][0]["op"][1]["new_parameters"], indent=4))
+    new_fees = tx["operations"][0][1]["proposed_ops"][0]["op"][1]["new_parameters"]["current_fees"]
+
+    print(json.dumps(DeepDiff(old_fees, new_fees), indent=4))
 
     if not broadcast:
         print("=" * 80)
