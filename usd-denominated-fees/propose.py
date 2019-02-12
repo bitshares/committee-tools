e512bfcea7513ab69340c602853473cc785d0ceb
Fabian Schuh <mail@xeroc.org>
2019-02-12 13:54:29 +0100

    Revert "fix propose script"
    This reverts commit 1392bd3323d30f54d5fd557ff53f7e77650d00b5.

--- usd-denominated-fees/propose.py
+++ usd-denominated-fees/propose.py
@@ -1,35 +1,28 @@
-import time
-import click
-import config
 from grapheneapi.grapheneapi import GrapheneAPI
 from bitshares.market import Market
 from pprint import pprint
 from datetime import datetime
+import time
 from deepdiff import DeepDiff
+import config
 
 
-@click.command()
-@click.option("--rpchost", default="localhost")
-@click.option("--rpcport", default=8092)
-@click.option("--expiration", default=60 * 60 * 24 * 14)
-@click.option("--broadcast/--no-broadcast", default=False)
-@click.option("--proposer", default="xeroc")
-def main(rpchost, rpcport, expiration, broadcast, proposer):
-    rpc = GrapheneAPI(rpchost, rpcport)
-    expiration = datetime.utcfromtimestamp(time.time() + int(expiration)).strftime(
-        "%Y-%m-%dT%H:%M:%S"
-    )
+if __name__ == "__main__":
+    graphene = GrapheneAPI(config.wallet_host, config.wallet_port)
+    expiration = datetime.utcfromtimestamp(
+        time.time() + int(config.expires_from_now)
+    ).strftime("%Y-%m-%dT%H:%M:%S")
 
     # Get current fees
-    obj = rpc.get_object("2.0.0")[0]
+    obj = graphene.get_object("2.0.0")[0]
     old_fees = obj["parameters"]["current_fees"]
     scale = int(obj["parameters"]["current_fees"]["scale"]) / 1e4
-    core_asset = rpc.get_asset("1.3.0")
+    core_asset = graphene.get_asset("1.3.0")
 
     # Get ticker/current price
     market = Market(config.market)
     ticker = market.ticker()
-    # core_exchange_rate = float(ticker["core_exchange_rate"])
+    core_exchange_rate = float(ticker["core_exchange_rate"])
     settlement_price = float(ticker["quoteSettlement_price"])
 
     # Translate native fee in core_asset fee
@@ -49,7 +42,9 @@ def main(rpchost, rpcport, expiration, broadcast, proposer):
                     * settlement_price
                 )
 
-    tx = rpc.propose_fee_change(proposer, expiration, new_fees, broadcast)
+    tx = graphene.propose_fee_change(
+        config.proposer, expiration, new_fees, config.broadcast
+    )
     new_fees = tx["operations"][0][1]["proposed_ops"][0]["op"][1]["new_parameters"][
         "current_fees"
     ]
@@ -57,10 +52,6 @@ def main(rpchost, rpcport, expiration, broadcast, proposer):
     # Show differences from previous to new fees
     pprint(DeepDiff(old_fees, new_fees))
 
-    if not broadcast:
+    if not config.broadcast:
         print("=" * 80)
-        print("Set --broadcast if the transaction shall be broadcast!")
-
-
-if __name__ == "__main__":
-    main()
+        print("Set broadcast to 'True' if the transaction shall be broadcast!")
